7723d6e79a566b844448c6bf7a753f24f40d7d4c
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-08 14:59:33 +0100

    🌍 Made an update

--- scripts/warp-init.sh
+++ scripts/warp-init.sh
@@ -1,4 +1,5 @@
 #! /bin/sh
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
 # export STARSHIP_CACHE=~/.cache/starship/
+export STARSHIP_CACHE=~/.config/starship/cache
 eval "$(starship init zsh)"
