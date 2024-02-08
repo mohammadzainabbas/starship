1d3d6f37203e64515c2f9b40cd28766ac9800b7a
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-08 15:24:24 +0100

    🐛 Fixed minor typos in an update

--- scripts/warp-init.sh
+++ scripts/warp-init.sh
@@ -2,6 +2,7 @@
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
 # export STARSHIP_CACHE=~/.cache/starship/
 export STARSHIP_CACHE=~/.config/starship/cache/
+# https://github.com/starship/starship/blob/master/src/init/starship.zsh
 eval "$(starship init zsh)"
 export STARSHIP_SHELL="zsh-warp"
 export STARSHIP_SESSION_KEY="warp ${STARSHIP_SESSION_KEY}"
