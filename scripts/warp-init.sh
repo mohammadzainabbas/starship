e32a33d5699beeb8ccf60e84daeb62fbfc90d031
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-08 14:46:24 +0100

    🧱 Laid groundwork for a module

--- scripts/warp-init.sh
+++ scripts/warp-init.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
-export STARSHIP_CONFIG=~/.config/starship.old.toml
+export STARSHIP_CONFIG=~/.config/starship/starship.toml
 # export STARSHIP_CACHE=~/.cache/starship/
 eval "$(starship init zsh)"
