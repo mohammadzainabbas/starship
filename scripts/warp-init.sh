dfab84e952dfd55d1ad383d873b2570cb76c3c3f
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-02-08 16:11:00 +0100

    🚑 Hotfix applied to resolve critical issue

--- scripts/warp-init.sh
+++ scripts/warp-init.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 # export STARSHIP_CONFIG=~/.config/starship/starship.toml
-export STARSHIP_CONFIG=~/.config/starship/themes/tokyo-night.toml
+export STARSHIP_CONFIG=~/.config/starship/themes/pastel-powerline.toml
 # export STARSHIP_CACHE=~/.cache/starship/
 export STARSHIP_CACHE=~/.config/starship/cache/
 # https://github.com/starship/starship/blob/master/src/init/starship.zsh
