6ffc7ec1f8960deb06beefc56d5b1d883b0ef6f6
Mohammad Zain Abbas <mohammadzainabbas@gmail.com>
2024-04-15 10:02:52 +0200

    Minor change

--- scripts/warp-init.sh
+++ scripts/warp-init.sh
@@ -15,7 +15,7 @@
 export STARSHIP_CONFIG=~/.config/starship/starship.toml
 # export STARSHIP_CACHE=~/.cache/starship/
 # export STARSHIP_CACHE=~/.config/starship/cache/
-# https://github.com/starship/starship/blob/master/src/init/starship.zsh
+# Check (https://github.com/starship/starship/blob/master/src/init/starship.zsh) for startup script for zsh shell
 eval "$(starship init zsh)"
 export STARSHIP_SHELL="zsh-warp"
 export STARSHIP_SESSION_KEY="warp ${STARSHIP_SESSION_KEY}"
